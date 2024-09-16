431ceee1746431367baeb108906ce7d413713769
nytouu <capitaine7312@gmail.com>
2024-09-17 01:03:08 +0200

    reenable picom & fontconfig files

--- home-manager/files.nix
+++ home-manager/files.nix
@@ -5,9 +5,9 @@
     file = {
       # ".config/htop".source = ./config/htop;
       # ".config/lazygit".source = ./config/lazygit;
-      # ".config/fontconfig".source = ./config/fontconfig;
+      ".config/fontconfig".source = ./config/fontconfig;
 
-      # ".config/picom.conf".source = ./config/picom.conf;
+      ".config/picom.conf".source = ./config/picom.conf;
       # ".config/rofi".source = ./config/rofi;
     };
   };
